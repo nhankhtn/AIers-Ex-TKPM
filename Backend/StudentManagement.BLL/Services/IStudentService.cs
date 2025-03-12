924b47cd3cc896406e937a4c47d48de5276cea8b
tranngocnhan <tranngocnhannt2004@gmail.com>
2025-03-12 23:40:57 +0700

    add folder backend

---
+++ Backend/StudentManagement.BLL/Services/IStudentService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using StudentManagement.BLL.DTOs;
+using StudentManagement.Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.BLL.Services
+{
+    public interface IStudentService
+    {
+        Task<Result<IEnumerable<StudentDTO>>> GetAllStudentsAsync();
+
+        Task<Result<string>> AddStudentAsync(StudentDTO studentDTO);
+
+        Task<Result<string>> DeleteStudentAsync(string studentId);
+
+        Task<Result<string>> UpdateStudentAsync(string userId, UpdateStudentDTO studentDTO);
+
+        Task<Result<StudentDTO?>> GetStudentByIdAsync(string studentId);
+
+        Task<Result<IEnumerable<StudentDTO>>> GetStudentsByNameAsync(string name);
+    }
+}
